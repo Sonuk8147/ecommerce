087205a3c31c77a6d49e44cd364a232b7bfba599
Sonu kumar <sonu1993kr@gmail.com>
2018-06-18 18:56:42 +0530

    Included Bootstrap Simplex Theme as well.

---
+++ frontend/src/main/webapp/assets/js/myApp.js
@@ -0,0 +1,18 @@
+$(function(){
+	//solving the active menu problem
+	switch(menu){
+		
+	case 'About us':
+		$('#about').addClass('active');
+		break;
+	case 'Contact':
+		$('#contact').addClass('active');
+		break;
+	default:
+		$('#home').addClass('active');
+		break;		
+	
+	}
+	
+	
+});
